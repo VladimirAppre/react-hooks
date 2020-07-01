e7fd69066723631d7c97d08e15d6875514af1055
Vladimir K <vladimir.appre@gmail.com>
2020-07-01 15:20:41 +0300

    useState

---
+++ src/use-state.js
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+import ReactDom from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <HookSwitcher/>
+    </div>
+  );
+};
+
+const HookSwitcher = () => {
+  const [color, setColor] = useState('gray');
+  const [fontSize, setFontSize] = useState(14);
+
+  return (
+    <div style={{
+      padding: '10px',
+      backgroundColor: color,
+      fontSize: `${fontSize}px`
+    }}>
+      Use State hook
+      <button
+        onClick={() => {
+          setColor('gray')
+        }}>Dark
+      </button>
+      <button
+        onClick={() => {
+          setColor('white')
+        }}>Light
+      </button>
+      <button
+        onClick={() => setFontSize((s) => s + 2)}>
+        + 2px Size
+      </button>
+    </div>
+  );
+};
+
+ReactDom.render(<App/>, document.getElementById('root'));
